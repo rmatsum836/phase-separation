d3f94fa302b2a0b497e8057aaef6f0cf784dbcb3
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-04-23 09:02:57 -0500

    Add functions for thresholding and k-means clustering

--- image_process.py
+++ image_process.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cv2
+from sklearn.cluster import KMeans
+from skimage.transform import resize
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -29,14 +32,34 @@ def convolveImage(image, kernel):
     return convolved.astype(int)
 
 
-def apply_otsu(gray, out_file=None, save=True):
+def apply_otsu(gray,dom_color,out_file=None, save=True):
     thresh_otsu = threshold_otsu(gray)
-    im_bw = gray > thresh_otsu
-    
+    if dom_color == 2:
+        im_bw = gray > thresh_otsu
+    elif dom_color == 0:
+        im_bw - gray < thresh_otsu
+
     if save == True:
         imsave(out_file, gray)
 
     return im_bw
+
+
+def remove_border(image):
+    image[:,:90] = 0
+    image[:,-90:] = 0
+    image[:90,:] = 0
+    image[-90:,:] = 0
+
+    return image
+
+
+def get_dominant_color(image, clusters=1):
+    dc = DominantColors(image, clusters)
+    colors = dc.dominantColors()
+    dominant = list(colors[0]).index(max(colors[0]))
+
+    return dominant
 
 
 def cutoff_particles(image, image_props, cutoff=300, out_file=None, save=None):
@@ -55,3 +78,61 @@ def cutoff_particles(image, image_props, cutoff=300, out_file=None, save=None):
     print('Number of individual regions = {}'.format(n_regions))
 
     return n_regions
+
+
+class DominantColors:
+
+    CLUSTERS = None
+    IMAGE = None
+    COLORS = None
+    LABELS = None
+
+    def __init__(self, image, clusters, filename):
+        self.CLUSTERS = clusters
+        self.IMAGE = image
+        self.FILE = filename
+
+    def dominantColors(self):
+
+        #read image
+        img = cv2.imread(self.IMAGE)
+        img = cv2.resize(img, dsize=(50, 50), interpolation=cv2.INTER_CUBIC)
+
+        #convert to rgb from bgr
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        #reshaping to a list of pixels
+        img = img.reshape((img.shape[0] * img.shape[1], 3))
+
+        #save image after operations
+        self.IMAGE = img
+
+        #using k-means to cluster pixels
+        kmeans = KMeans(n_clusters = self.CLUSTERS)
+        kmeans.fit(img)
+
+
+        #the cluster centers are our dominant colors.
+        self.COLORS = kmeans.cluster_centers_
+
+        #save labels
+        self.LABELS = kmeans.labels_
+
+
+        #returning after converting to integer from float
+        return self.COLORS.astype(int)
+
+    def rgb_to_hex(self, rgb):
+        return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2])) 
+    def plotClusters(self):
+        fig = plt.figure(figsize=(10,8))
+        ax = Axes3D(fig)
+        for label, pix in zip(self.LABELS, self.IMAGE):
+            ax.scatter(pix[0], pix[1], pix[2], color = self.rgb_to_hex(self.COLORS[label]))
+        ax.set_xlabel('Red', fontsize=18, labelpad=13)
+        ax.set_ylabel('Green', fontsize=18, labelpad=13)
+        ax.set_zlabel('Blue', fontsize=18, labelpad=16)
+        ax.tick_params(axis = 'both', which = 'major', labelsize = 18)
+        plt.tight_layout()
+        plt.savefig(self.FILE)
+        plt.show()
