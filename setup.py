411feee3fecf6024cbc7d4f7d706eda90026b0b7
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-05-12 09:22:26 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(
+        name = "phase-separation",
+        version = "0.1.0"
+        author = "Ray Matsumoto",
+        author_email = "ray.a.matsumoto@vanderbilt.edu",
+        description = ("Functions to detect phase separation in bulk
+        solutions")
+)
