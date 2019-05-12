ab4d7682beac0e95c30127074ab7c6cdc1b30710
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2019-05-12 10:43:32 -0500

    Fix typo in setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name = "phase-separation",
-        version = "0.1.0"
+        version = "0.1.0",
         author = "Ray Matsumoto",
         author_email = "ray.a.matsumoto@vanderbilt.edu",
         description = ("Functions to detect phase separation in bulk
